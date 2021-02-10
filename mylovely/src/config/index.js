ae3dddfe04372b79867fb7b5b675fabc59435086
an <48993744+AnfraRa1n@users.noreply.github.com>
2021-02-10 14:22:32 +0700

    Add files via upload

---
+++ mylovely/src/config/index.js
@@ -0,0 +1,20 @@
+export default {
+  ucapan: [
+    "Bagaimana hidup kamu, tetaplah hidup !",
+    "Harta, Tahta, Nurfadilah >.<",
+    "Hey! I wuv yuuu! UwU",
+  ],
+  panggilan: ["Padilyyy"],
+  colors: [
+    "#FF6900",
+    "#FCB900",
+    "#7BDCB5",
+    "#00D084",
+    "#8ED1FC",
+    "#0693E3",
+    "#ABB8C3",
+    "#EB144C",
+    "#F78DA7",
+    "#000000",
+  ],
+};
